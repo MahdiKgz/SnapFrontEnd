8a7f91d05101bf67488c636c301160b1f056d0d5
mahdi.khoshghadam <mahdikgz7@gmail.com>
2026-07-19 17:25:50 +0330

    chore: remove redundant file

--- src/app/styles/antd-theme.ts
+++
@@ -1,9 +0,0 @@
-import type { ThemeConfig } from "antd";
-
-export const antdTheme: ThemeConfig = {
-  token: {
-    colorPrimary: "#1677ff",
-    borderRadius: 6,
-    fontFamily: "inherit",
-  },
-};
